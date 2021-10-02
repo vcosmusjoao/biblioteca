da575f96f3a4aab0948032f244746c0b397c3664
lolkura43 <leonardo.recurso@outlook.com>
2021-10-02 15:04:35 -0300

    metodo cadastro usuario

---
+++ src/main/java/br/com/letscode/java/biblioteca/usuario/CadastroUsuario.java
@@ -0,0 +1,17 @@
+package br.com.letscode.java.biblioteca.usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroUsuario {
+
+    private List<Usuario> usuarios = new ArrayList<>();
+    public void adicionarUsuario(Usuario usuario) {
+        this.usuarios.add(usuario);
+    }
+
+    public List<Usuario> listarUsuario(){
+        return usuarios;
+    }
+    
+}
